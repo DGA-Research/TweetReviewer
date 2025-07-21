cd1295cab2a1c816bfa022ae0cdaa635cbb2fda0
DGA-Research <dgaresearchcoding@gmail.com>
2025-07-21 15:36:06 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import re
+import pandas as pd
+from datetime import datetime
+from docx import Document
+from docx.shared import Pt
+from docx.enum.style import WD_STYLE_TYPE
+from docx.oxml import OxmlElement
+from docx.oxml.ns import qn
+import webbrowser
+
+# user interface
+
+
+Upload CSV
+
+For row in __ etc.
+
+Need to have a new file to write to
+
+# Options:
+  Twitter Handle
+  Bad words or not
+
+
